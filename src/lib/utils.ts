e6f6fb81afec84f5a03e78af3d008f241cfe6466
TD STUDIOS <tyler@tdstudiosny.com>
2025-07-17 00:04:08 -0500

    [
    p

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,9 @@
+import { badgeVariants } from "@/components/ui/badge-variants"
+import type { VariantProps } from "class-variance-authority"
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+export type BadgeVariantsProps = VariantProps<typeof badgeVariants>
